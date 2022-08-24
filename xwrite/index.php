10009a597eb4a17bc2e26714c2519eee832e5c93
PHVlasov <111577899+PHVlasov@users.noreply.github.com>
2022-08-24 16:25:02 +0300

    Add files via upload

---
+++ xwrite/index.php
@@ -0,0 +1,13 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+//header("Content-Type: application/json; charset=UTF-8");
+header("Cache-Control: no-store, max-age=0");
+?>
+<?php
+
+require __DIR__."/../engine/xpostproc.php";
+
+?>
+
+
+
